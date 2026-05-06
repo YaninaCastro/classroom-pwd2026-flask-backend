9c3a4e01ce96870c0c1c53fa7fbc3ead5eac0d89
YaninaCastro <yanicastrouni@gmail.com>
2026-05-06 02:59:32 -0300

    Update movimiento_stock_routes.py

--- backend/app/routes/movimiento_stock_routes.py
+++ backend/app/routes/movimiento_stock_routes.py
@@ -5,21 +5,21 @@ from app.decorators.rol_access import rol_access
 movimientos_stock = Blueprint('movimientos_stock', __name__, url_prefix='/movimientos')
 
 
-# GET /movimientos/ → solo admin
+# GET /movimientos/: solo admin
 @movimientos_stock.route('/')
 @rol_access('admin')                          #Filtro usuario con el decorador de rol: @rol_accss('')
 def get_all():
     return MovimientoStockController.get_all()
 
 
-# GET /movimientos/mis → usuario autenticado (admin u operador)
+# GET /movimientos/mis: usuario autenticado (admin u operador)
 @movimientos_stock.route('/mis')
 @rol_access('admin', 'operador')
 def mis_movimientos():
     return MovimientoStockController.mis_movimientos()
 
 
-# POST /movimientos/ → usuario autenticado (admin u operador)
+# POST /movimientos/: usuario autenticado (admin u operador)
 @movimientos_stock.route("/", methods=['POST'])
 @rol_access('admin', 'operador')
 def create():
