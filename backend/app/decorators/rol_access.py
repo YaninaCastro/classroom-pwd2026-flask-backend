cc240092fae69f5a6e7f954a2f1662538b052a40
YaninaCastro <yanicastrouni@gmail.com>
2026-05-06 02:58:06 -0300

    rol_access.py

--- backend/app/decorators/rol_access.py
+++ backend/app/decorators/rol_access.py
@@ -1,35 +1,3 @@
-'''
-from functools import wraps
-from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
-from app.database import db
-from app.models.user import User
-from flask import jsonify
-
-
-def rol_access(*roles_permitidos):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-
-            # 🔒 verifica que haya token válido
-            verify_jwt_in_request()
-
-            user_id = get_jwt_identity()
-
-            user = db.session.execute(
-                db.select(User).filter_by(id=user_id)
-            ).scalar_one_or_none()
-
-            if user and user.rol and user.rol.nombre in roles_permitidos:
-                return func(*args, **kwargs)
-
-            return jsonify({
-                'message': 'Acceso denegado: rol no autorizado'
-            }), 403
-
-        return wrapper
-    return decorator
-'''
 
 from functools import wraps
 from flask import jsonify
